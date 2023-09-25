9fab0f04640058c58bca654de158a37a7e7e2054
Edgar <tuyisengeedgar@gmail.com>
2023-09-25 14:22:46 +0300

    changing checkin component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/checkin";
+import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./context/DarkModeContext";
 
